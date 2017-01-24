0f696647b18ee982c0118ee80947936f2e1e8903
Andrés Lucas García Fiorini <agf234@users.noreply.github.com>
2017-01-24 14:13:19 -0300

    Update DEPLOY.sh

--- DEPLOY.sh
+++ DEPLOY.sh
@@ -10,9 +10,34 @@ echo "##############################"
 echo "##############################"
 
 yes admin | bosh target $BOSH_TARGET
+if [ $? != 0 ];
+then
+     echo "##############################"
+     echo "bosh target error"
+     exit 249;
+fi
 
 bosh deployment cf-manifest/minimal-aws-salida.yml
+if [ $? != 0 ];
+then
+     echo "##############################"
+     echo "bosh deployment error"
+     exit 248;
+fi
 bosh upload stemcell https://s3.amazonaws.com/bosh-aws-light-stemcells/light-bosh-stemcell-3312.15-aws-xen-hvm-ubuntu-trusty-go_agent.tgz
+
 bosh create release
+if [ $? != 0 ];
+then
+     echo "##############################"
+     echo "bosh create release error"
+     exit 247;
+fi
 bosh upload release
+if [ $? != 0 ];
+then
+     echo "##############################"
+     echo "bosh upload release error"
+     exit 246;     
+fi
 # bosh deploy
