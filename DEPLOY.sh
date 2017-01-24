726a51de480b2b4ce664ba094668fc5371215f58
Andrés Lucas García Fiorini <agf234@users.noreply.github.com>
2017-01-24 14:08:10 -0300

    Update DEPLOY.sh

--- DEPLOY.sh
+++ DEPLOY.sh
@@ -11,7 +11,8 @@ rvm
 rvm list
 echo "##############################"
 
-bosh target $BOSH_TARGET
+yes admin | bosh target $BOSH_TARGET
+
 bosh deployment cf-manifest/minimal-aws-salida.yml
 bosh upload stemcell https://s3.amazonaws.com/bosh-aws-light-stemcells/light-bosh-stemcell-3312.15-aws-xen-hvm-ubuntu-trusty-go_agent.tgz
 bosh create release
