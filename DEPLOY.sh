65581989473b9f063af76fb2c4547a1d8469e21d
Andrés Lucas García Fiorini <agf234@users.noreply.github.com>
2017-01-24 13:51:16 -0300

    Update DEPLOY.sh

--- DEPLOY.sh
+++ DEPLOY.sh
@@ -5,11 +5,10 @@
 # Altoros S.A.(Argentina)
 
 /bin/ls -ltr
-bosh status
-BOSH_W=`/usr/bin/whereis bosh`
 
-$BOSH_W deployment cf-manifest/minimal-aws-salida.yml
-$BOSH_W upload stemcell https://s3.amazonaws.com/bosh-aws-light-stemcells/light-bosh-stemcell-3312.15-aws-xen-hvm-ubuntu-trusty-go_agent.tgz
-$BOSH_W create release
-$BOSH_W upload release
-
+bosh target $BOSH_TARGET
+bosh deployment cf-manifest/minimal-aws-salida.yml
+bosh upload stemcell https://s3.amazonaws.com/bosh-aws-light-stemcells/light-bosh-stemcell-3312.15-aws-xen-hvm-ubuntu-trusty-go_agent.tgz
+bosh create release
+bosh upload release
+# bosh deploy
